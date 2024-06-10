2af6f199f06c74b6505527df095312f077798b78
nekar5 <mrkareo@gmail.com>
2024-06-10 17:03:39 +0300

    release test

--- app/src/main/java/com/example/trainingcompanion/ui/adapter/ExerciseShortAdapter.java
+++ app/src/main/java/com/example/trainingcompanion/ui/adapter/ExerciseShortAdapter.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +19,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.trainingcompanion.R;
 import com.example.trainingcompanion.data.model.Exercise;
 import com.example.trainingcompanion.extra.CustomChronometer;
-import com.example.trainingcompanion.extra.NotificationHelper;
 
 import java.util.ArrayList;
 import java.util.List;
